3e3aab1338c32fa10710cb52b4a892d35699fbfb
Shivans <rajshivans85@gmail.com>
2023-05-17 19:51:47 +0530

    commit

---
+++ routes/pages.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const loggedIn = require("../controllers/loggedIn")
+const logout = require("../controllers/logout")
+const router = express.Router();
+
+router.get("/", loggedIn, (req,res) => {
+    if(req.user) {
+        res.render("index", { status: "loggedIn", user: req.user });
+    }else {
+        res.render("index", { status: "no", user: "nothing" } );
+    }
+})
+router.get("/register", (req,res)=>{
+    res.sendFile("register.html", { root: "./public"});
+})
+router.get("/login", (req,res)=> {
+    res.sendFile("login.html", {root: "./public"});
+})
+router.get('/profile', loggedIn, (req, res) => {
+    if (req.user) {
+        res.sendFile("profile.html", { root: "./public" })
+    } else {
+        res.sendFile("login.html", { root: "./public" });
+    }
+})
+
+router.get("/logout", logout)
+module.exports = router;
